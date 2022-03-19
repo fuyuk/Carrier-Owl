fabb5c2513679ffe2312b9ea70b47508f9a88cac
fuyuk <44751334+fuyuk@users.noreply.github.com>
2022-03-19 18:19:13 +0900

    Update carrier_owl.py
    バグを取りました

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -178,26 +178,27 @@ def main():
     keywords = config['keywords']
     score_threshold = float(config['score_threshold'])
 
-    day_before_yesterday = datetime.datetime.today() - datetime.timedelta(days=2)
-    day_before_yesterday_str = day_before_yesterday.strftime('%Y%m%d')
-    # datetime format YYYYMMDDHHMMSS
-    arxiv_query = f'({subject}) AND ' \
-                  f'submittedDate:' \
-                  f'[{day_before_yesterday_str}000000 TO {day_before_yesterday_str}235959]'
-    articles = arxiv.query(query=arxiv_query,
-                           max_results=1000,
-                           sort_by='submittedDate',
-                           iterative=False)
-    results = search_keyword(articles, keywords, score_threshold)
+#     day_before_yesterday = datetime.datetime.today() - datetime.timedelta(days=2)
+#     day_before_yesterday_str = day_before_yesterday.strftime('%Y%m%d')
+#     # datetime format YYYYMMDDHHMMSS
+#     arxiv_query = f'({subject}) AND ' \
+#                   f'submittedDate:' \
+#                   f'[{day_before_yesterday_str}000000 TO {day_before_yesterday_str}235959]'
+#     articles = arxiv.query(query=arxiv_query,
+#                            max_results=1000,
+#                            sort_by='submittedDate',
+#                            iterative=False)
+#     results = search_keyword(articles, keywords, score_threshold)
 
     slack_id = os.getenv("SLACK_ID") or args.slack_id
     line_token = os.getenv("LINE_TOKEN") or args.line_token
+    # notify(results, slack_id, line_token)
     
     results = check_reservation()
     if results is None:
-        notify("空きがあります。", slack_id, line_token)
+        send2app("空きがあります。", slack_id, line_token)
     else:
-        notify("空きがありません。", slack_id, line_token)  
+        send2app("空きがありません。", slack_id, line_token)  
 
 if __name__ == "__main__":
     main()
